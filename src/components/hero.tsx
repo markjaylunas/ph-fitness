93ab16d82c84e4309fc5fd6d17815a2f87d07b02
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-09-10 13:20:38 +0800

    remove unused

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="relative flex flex-col-reverse sm:flex-row max-h-screen max-w-screen-2xl mx-auto px-4 sm:px-32">
+    <section className="relative flex flex-col-reverse sm:flex-row gap-8 max-h-screen max-w-screen-2xl mx-auto px-4 sm:px-32">
       <div className="flex-1 flex flex-col justify-center gap-4">
         <h2 className="flex flex-col scroll-m-20 text-4xl font-semibold tracking-tight lg:text-6xl">
           <span>Build your</span>
@@ -31,7 +31,7 @@ export default function Hero() {
 
       <div className="flex-1 flex justify-end items-center">
         <div className="relative z-0 h-3/4">
-          <h1 className="absolute -z-10 top-1/2 left-[32%] sm:left-[35%] -rotate-90 transform -translate-x-1/2 -translate-y-1/2 text-5xl md:text-6xl lg:text-8xl font-black text-white/25 text-nowrap">
+          <h1 className="absolute -z-10 top-1/2 left-[32%] -rotate-90 transform -translate-x-1/2 -translate-y-1/2 text-5xl md:text-6xl lg:text-8xl font-black text-white/25 text-nowrap">
             PH Fitness
           </h1>
           <Image
@@ -40,7 +40,6 @@ export default function Hero() {
             className="brightness-75 max-h-[350px] sm:max-h-full object-contain"
             unoptimized
           />
-          {/* <div className="absolute top-0 right-0 z-10 h-full w-full bg-black/20" /> */}
         </div>
       </div>
     </section>
