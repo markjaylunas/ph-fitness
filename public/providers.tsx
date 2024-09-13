e11833c8a147f9e3bd4fe10714ee37d0104918fe
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-09-13 15:08:12 +0800

    update meta and remove unused

--- public/providers.tsx
+++
@@ -1,6 +0,0 @@
-import { NextUIProvider } from "@nextui-org/react";
-import { ReactNode } from "react";
-
-export default async function Providers({ children }: { children: ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
-}
